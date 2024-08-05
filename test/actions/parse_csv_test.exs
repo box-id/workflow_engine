6fc3d1c3d16b841f18c039bb2cc8f3657acbd468
Stefan Fochler <istefo@me.com>
2024-08-05 08:13:50 +0200

    Parse CSV: make build_workflow helper accept all possible options

--- test/actions/parse_csv_test.exs
+++ test/actions/parse_csv_test.exs
@@ -9,7 +9,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
   describe "Parse CSV Action" do
     test "Parse CSV with comma" do
       {:ok, result} =
-        build_workflow(",")
+        build_workflow(csv_settings: [separator: ","])
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "name,age\nJohn,30\nJane,25"
@@ -26,7 +26,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Parse CSV with semicolon" do
       {:ok, result} =
-        build_workflow(";")
+        build_workflow(csv_settings: [separator: ";"])
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "name;age\nJohn;30\nJane;25"
@@ -43,7 +43,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Parse CSV with tab" do
       {:ok, result} =
-        build_workflow("\t")
+        build_workflow(csv_settings: [separator: "\t"])
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "name\tage\nJohn\t30\nJane\t25"
@@ -60,7 +60,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Parse CSV with empty file" do
       {:ok, result} =
-        build_workflow(nil)
+        build_workflow()
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => ""
@@ -74,7 +74,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Parse CSV with utf encoding" do
       {:ok, result} =
-        build_workflow(",")
+        build_workflow()
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "name,age\nJöhn,30\nJäne,25"
@@ -91,7 +91,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Parse CSV with missing values" do
       {:ok, result} =
-        build_workflow(",")
+        build_workflow()
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "name,age\nJohn,\nJane,25"
@@ -108,7 +108,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Parse CSV with quotation marks" do
       {:ok, result} =
-        build_workflow(",")
+        build_workflow()
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => ~s'name,age\n"""John""",30\nJane,25'
@@ -125,7 +125,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Parse CSV with BOM character" do
       {:ok, result} =
-        build_workflow(",")
+        build_workflow()
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => <<239, 187, 191>> <> "name,age\nJohn,30\nJane,25"
@@ -140,9 +140,9 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
              ]
     end
 
-    test "Parse presplit CSV with BOM character" do
+    test "Parse pre-split CSV with BOM character" do
       {:ok, result} =
-        build_workflow(",")
+        build_workflow()
         |> WorkflowEngine.evaluate(
           params: %{
             # Not sure if this input format (with newline terminated rows in a list...) is
@@ -163,8 +163,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
   describe "Parse CSV Action - Columns option" do
     test "uses manually supplied columns of header-less CSV with number of fields matching" do
       {:ok, result} =
-        build_workflow(",")
-        |> put_in(["steps", Access.at(0), "columns"], ["name", "age"])
+        build_workflow(columns: ["name", "age"])
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "John,30\nJane,25\n"
@@ -181,8 +180,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "uses manually supplied columns, cutting off extra data fields" do
       {:ok, result} =
-        build_workflow(",")
-        |> put_in(["steps", Access.at(0), "columns"], ["name", "age"])
+        build_workflow(columns: ["name", "age"])
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "John,30,Plumber\nJane,25,Electrician"
@@ -199,8 +197,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Manually supplied columns with missing data fields" do
       {:ok, result} =
-        build_workflow(",")
-        |> put_in(["steps", Access.at(0), "columns"], ["name", "age", "profession"])
+        build_workflow(columns: ["name", "age", "profession"])
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "John,30\nJane,25\n"
@@ -217,9 +214,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
 
     test "Manually supplied columns and skip_header option" do
       {:ok, result} =
-        build_workflow(",")
-        |> put_in(["steps", Access.at(0), "columns"], ["name", "age"])
-        |> put_in(["steps", Access.at(0), "csv_settings", "skip_header"], true)
+        build_workflow(columns: ["name", "age"], csv_settings: [skip_header: true])
         |> WorkflowEngine.evaluate(
           params: %{
             "csv_data" => "Vorname,Alter\nJohn,30\nJane,25\n"
@@ -235,7 +230,7 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
     end
   end
 
-  defp build_workflow(separator) do
+  defp build_workflow(opts \\ []) do
     %{
       "steps" => [
         %{
@@ -243,12 +238,25 @@ defmodule WorkflowEngine.Actions.ParseCsvTest do
           "data" => %{
             "var" => "params.csv_data"
           },
-          "csv_settings" => %{
-            "separator" => separator
-          },
           "result" => %{"as" => "content_rows"}
         }
+        |> deep_merge(to_binary_map(opts))
       ]
     }
+  end
+
+  # Depp-convert maps and keyword lists to maps with binary keys.
+  defp to_binary_map(map) do
+    Map.new(map, fn {k, v} ->
+      {Atom.to_string(k),
+       if(is_map(v) or match?([{_k, _v} | _], v), do: to_binary_map(v), else: v)}
+    end)
+  end
+
+  defp deep_merge(map1, map2) do
+    Map.merge(map1, map2, fn
+      _key, v1, v2 when is_map(v1) and is_map(v2) -> deep_merge(v1, v2)
+      _key, _v1, v2 -> v2
+    end)
   end
 end

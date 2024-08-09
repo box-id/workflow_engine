0c18f46547f60707278955a70b8b2c132e89b72d
Stefan Fochler <istefo@me.com>
2024-08-09 16:34:27 +0200

    Bump version to 0.7.0 (release parse_csv changes)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WorkflowEngine.MixProject do
   def project do
     [
       app: :workflow_engine,
-      version: "0.6.1",
+      version: "0.7.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
